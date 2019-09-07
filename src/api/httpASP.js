51f5b2648edb0a14539eefb97dab792a7ee85b48
Cyril5169 <52475487+Cyril5169@users.noreply.github.com>
2019-09-07 16:26:21 +0800

    更正地址

--- src/api/httpASP.js
+++ src/api/httpASP.js
@@ -1,8 +1,9 @@
 import Axios from 'axios'
 
 //var baseUrl = 'http://localhost:49438/';//本地
-//var baseUrl = 'http://47.107.56.156:666/';//测试
-var baseUrl = 'http://47.107.56.156:568/';//正式
+var baseUrl = 'http://47.107.56.156:666/';//测试
+//var baseUrl = 'http://14.29.223.114:568/';//正式
+//var baseUrl = 'http://47.107.56.156:568/';//不用了
 
 /**
  * 封装get方法
