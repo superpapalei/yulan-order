26b007de6670c99f5dadd3a63a981b787d3b0e29
Cyril5169 <516974551@qq.com>
2019-10-14 18:49:54 +0800

    s

--- src/api/paymentASP.js
+++ src/api/paymentASP.js
@@ -1,7 +1,16 @@
 import { get, post, patch, put } from './httpASP'
+//银行汇款，委托喷绘，退货赔偿都放在这里
 //根据id查找汇款单
 export function GetPaymentById(data, config = {}) {
     return post('/PAYMENT_BILL/GetPaymentById', data, config).then((res) => {
+        return Promise.resolve(res);
+    }).catch((err) => {
+        return Promise.reject(err);
+    })
+}
+//插入委托喷绘记录
+export function InsertBrush(data, config = {}) {
+    return post('/AIRBRUSHDESIGNREASSURE/InsertBrush', data, config).then((res) => {
         return Promise.resolve(res);
     }).catch((err) => {
         return Promise.reject(err);
